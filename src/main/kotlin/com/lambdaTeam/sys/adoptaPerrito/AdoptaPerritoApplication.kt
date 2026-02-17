27512d6020f123c4d498d1a36b93550573403d7d
Alexis de Jesús Arizmendi López <alex.arizmendilz@ciencias.unam.mx>
2026-02-17 13:03:20 -0600

    Configuración inicial del proyecto

---
+++ src/main/kotlin/com/lambdaTeam/sys/adoptaPerrito/AdoptaPerritoApplication.kt
@@ -0,0 +1,11 @@
+package com.lambdaTeam.sys.adoptaPerrito
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdoptaPerritoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdoptaPerritoApplication>(*args)
+}
---
+++ src/test/kotlin/com/lambdaTeam/sys/adoptaPerrito/AdoptaPerritoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lambdaTeam.sys.adoptaPerrito
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdoptaPerritoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
