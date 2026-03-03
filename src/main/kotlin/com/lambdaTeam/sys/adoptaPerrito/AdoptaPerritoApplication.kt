7f7e835a4e4f75ad7092ac72bd9cfe94406a896d
Alexis de Jesús Arizmendi López <alex.arizmendilz@ciencias.unam.mx>
2026-03-02 18:04:23 -0600

    Se agrega la lectura del archivo .env en la clase principal

--- src/main/kotlin/com/lambdaTeam/sys/adoptaPerrito/AdoptaPerritoApplication.kt
+++ src/main/kotlin/com/lambdaTeam/sys/adoptaPerrito/AdoptaPerritoApplication.kt
@@ -1,5 +1,6 @@
 package com.lambdaTeam.sys.adoptaPerrito
 
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,8 @@ import org.springframework.boot.runApplication
 class AdoptaPerritoApplication
 
 fun main(args: Array<String>) {
+	dotenv().entries().forEach {
+		System.setProperty(it.key, it.value)
+	}
 	runApplication<AdoptaPerritoApplication>(*args)
 }
