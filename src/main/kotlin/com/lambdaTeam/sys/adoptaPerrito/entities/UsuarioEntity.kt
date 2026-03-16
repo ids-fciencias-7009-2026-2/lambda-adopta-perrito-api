c31b157333186c9443636520ce3f1f15a11f88ae
Luis Méndez <lgma9677@gmail.com>
2026-03-15 21:15:21 -0600

    Creando package entities y agregando clase UsuarioEntity. Corre bien el proyecto hasta este punto

---
+++ src/main/kotlin/com/lambdaTeam/sys/adoptaPerrito/entities/UsuarioEntity.kt
@@ -0,0 +1,20 @@
+package com.lambdaTeam.sys.adoptaPerrito.entities
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Table
+import jakarta.persistence.Id
+
+@Entity
+@Table(name = "usuarios")
+
+data class UsuarioEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id_usuario: Int,
+    var nombre: String,
+    var correo: String,
+    var codigo_postal: String,
+    var contrasena: String
+)
