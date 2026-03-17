35d49e364be7d302b365f35f0a412d6812570841
@juanfranco26 <juanfranco26@ciencias.unam.mx>
2026-03-17 17:15:21 -0600

    UsuarioRepository Buscar por Usuario y contraseña

--- src/main/kotlin/com/lambdaTeam/sys/adoptaPerrito/repositories/UsuarioRepository.kt
+++ src/main/kotlin/com/lambdaTeam/sys/adoptaPerrito/repositories/UsuarioRepository.kt
@@ -1,7 +1,13 @@
 package com.lambdaTeam.sys.adoptaPerrito.repositories
 
 import com.lambdaTeam.sys.adoptaPerrito.entities.UsuarioEntity
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UsuarioRepository : CrudRepository<UsuarioEntity, Int> {
-}
+
+    @Query("select u from UsuarioEntity u where u.correo = :correo and u.contrasena = :contrasena")
+    fun findUserByPasswordAndEmail(correo: String, contrasena: String): UsuarioEntity?
+    }
