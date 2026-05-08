273cf5d00c8a55706e4990ee4e8f4cb15372863a
Brandon <brandonjr@ciencias.unam.mx>
2026-05-07 19:21:09 -0600

    feat: agregar mappers y DTOs para el módulo de animales

---
+++ src/main/kotlin/com/lambdaTeam/sys/adoptaPerrito/entities/AnimalEntityEF.kt
@@ -0,0 +1,15 @@
+package com.lambdaTeam.sys.adoptaPerrito.entities
+
+import com.lambdaTeam.sys.adoptaPerrito.domain.Animal
+
+fun Animal.toAnimalEntity(): AnimalEntity {
+    return AnimalEntity(
+        id_animal = this.id,
+        nombre = this.nombre,
+        especie = this.especie,
+        raza = this.raza,
+        descripcion = this.descripcion,
+        fotoUrl = this.fotoUrl,
+        codigo_postal = this.codigoPostal
+    )
+}
